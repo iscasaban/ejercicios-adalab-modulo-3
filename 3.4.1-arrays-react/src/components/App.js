f8f50774be73c98f0ffa47ffa9627d691bce1178
Inma Sánchez Casabán <iscasaban@gmail.com>
2022-01-16 21:14:56 +0100

    update

--- 3.4.1-arrays-react/src/components/App.js
+++ 3.4.1-arrays-react/src/components/App.js
@@ -1,9 +1,23 @@
 import '../styles/App.scss';
 
 function App() {
+  
+  const tasks = [
+    'Comprar harina, jamón y pan rallado',
+    'Hacer croquetas ricas',
+    'Ir a la puerta de un gimnasio',
+    'Comerme las croquetas mirando a la gente que entra en el gimnasio'
+  ];
+
+  for (let index = 0; index < tasks.length; index++) {
+    tasks.push(<li>{index}</li>)     
+  }
   return (
     <div>
-      <h1>Hello world!</h1>
+      <h1>Mi lista de tareas</h1>
+      <ol>
+        <li>{tasks}</li>
+      </ol>
     </div>
   );
 }
